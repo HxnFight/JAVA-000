b3262aaa3fb29f37db672395d5cfc23bbf7a1948
valjean <valjean@jiemogame.com>
2020-10-20 19:45:54 +0800

    1. 添加助记符

---
+++ Week_01/java/valjean/Hello.java
@@ -0,0 +1,10 @@
+package valjean;
+
+public class Hello {
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int i = 0; i < 10; i++) {
+            sum += i;
+        }
+    }
+}
