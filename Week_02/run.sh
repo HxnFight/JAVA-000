7901ddf4a12b3fc96e3de45aa9d8673d91905b8c
valjean <HxnFight@users.noreply.github.com>
2020-10-27 21:19:16 +0800

    1. 增加数据

--- Week_02/run.sh
+++ Week_02/run.sh
@@ -7,10 +7,25 @@ set -x
 
 __memory=128
 
-for xx in $(seq 1 5)
+#__gc_type=Useserialgc
+#__gc_type=UseParallelGC
+#__gc_type=UseConcMarkSweepGC
+__gc_type=UseG1GC
+__log_path_prefix=gclog/$__gc_type
+
+if [[ ! -e $__log_path_prefix ]]; then
+    mkdir $__log_path_prefix
+fi
+
+for xx in $(seq 1 6)
 do
+    __log_suffix=$__memory-$__gc_type
+
+    java  -XX:+$__gc_type  -Xmx${__memory}m    -XX:+PrintGCDetails -XX:+PrintGCDateStamps \
+        -Xloggc:./$__log_path_prefix/log-$__log_suffix \
+        GCLogAnalysis   > $__log_path_prefix/run-log-$__log_suffix 2>&1
+
     __memory=$(expr $__memory \* 2)
-    java  -Xmx${__memory}m    -XX:+PrintGCDetails -XX:+PrintGCDateStamps   GCLogAnalysis  > gc-log-$__memory  2>&1
 done
 
 
